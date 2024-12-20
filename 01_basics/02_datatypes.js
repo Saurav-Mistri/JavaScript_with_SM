5e17ba4d8c4d626e9c70962979825487438cf700
Saurav Mistri <67297217+Saurav-Mistri@users.noreply.github.com>
2024-12-20 14:56:36 +0530

    datatypes are done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strick"; // treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+    
+console.log("Mr SM");
+
+let name = "Mr SM"
+let age = 26
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Mr SM"); // string
+console.log(typeof age); // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
+
+
+
