f2d320c57afd8ccb218543a275d5bae9ab8a5950
webdev2 <webdev2@digicians.com>
2024-12-25 12:24:09 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversationOperations.js
@@ -0,0 +1,30 @@
+// let score = "MrSM"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Notes: 
+//"33" => 33
+// "33abc" => NaN
+// true => 1; fasle => 0
+
+// let isLoggedIn = "MrSM"
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// Notes: 
+// 1 => true; 0 => false
+// "" => false
+// "MrSM" => true
+
+let sumNumber = 34;
+
+let strNumber = String(sumNumber);
+
+console.log(strNumber);
+console.log(typeof strNumber);
