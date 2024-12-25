c705e01d81c7565d7c7b0a0b6347bd479662f8e8
webdev2 <webdev2@digicians.com>
2024-12-25 15:26:58 +0530

    Operations done

--- 01_basics/03_conversationOperations.js
+++ 01_basics/03_conversationOperations.js
@@ -26,5 +26,46 @@ let sumNumber = 34;
 
 let strNumber = String(sumNumber);
 
-console.log(strNumber);
-console.log(typeof strNumber);
+// console.log(strNumber);
+// console.log(typeof strNumber);
+
+
+// *****************************************Operations********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(3**3);
+// console.log(2%2);
+
+
+let str1 = "Hello";
+let str2 = " Mr SM";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
