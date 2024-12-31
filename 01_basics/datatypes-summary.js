4dc6f3c3467d0e8e429eed3b5426e23387e3781d
webdev2 <webdev2@digicians.com>
2024-12-31 22:39:10 +0530

    stack and heap memory allocation done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -13,7 +13,7 @@ let userEmail2 = undefined; // undefined
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 3453674859878435465965786n
 
@@ -32,15 +32,42 @@ const myFunction = function(){
     console.log("Hello world!");
     }
 
-console.log(typeof heros);
-console.log(typeof myObj);
-console.log(typeof myFunction);
-console.log(typeof bigNumber);
-console.log(typeof id);
-console.log(typeof anotherId);
-console.log(typeof userEmail);
-console.log(typeof userEmail2);
-console.log(typeof outSideTemp);
-console.log(typeof isLoggedIn);
-console.log(typeof score);
-console.log(typeof scoreValue);
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
+// console.log(typeof bigNumber);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log(typeof userEmail);
+// console.log(typeof userEmail2);
+// console.log(typeof outSideTemp);
+// console.log(typeof isLoggedIn);
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYouTubeName = "Studypoint"
+
+let anotherName = myYouTubeName
+
+anotherName = "chaiaurcode"
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "mrsm@gmail.com",
+    upi: "mrsm@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sm@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
