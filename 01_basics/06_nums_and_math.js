dc4db4b6c13b7576ab9088037d7566fd113dc8bd
webdev2 <webdev2@digicians.com>
2025-01-01 20:47:12 +0530

    Number datatype is done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+console.log(score);
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 35.556
+
+console.log(otherNumber.toPrecision(3));
+
+const hundredsNum = 1000000
+
+console.log(hundredsNum.toLocaleString('en-IN'));
