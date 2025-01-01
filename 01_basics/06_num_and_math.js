6fe6216ca6f815cd754057a56f7eec351a6ef661
webdev2 <webdev2@digicians.com>
2025-01-01 22:24:05 +0530

    num and math are done

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 35.556
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundredsNum = 1000000
+
+// console.log(hundredsNum.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.abs(8));
+// console.log(Math.round(93.3));
+// console.log(Math.ceil(93.3));
+// console.log(Math.floor(92.9));
+// console.log(Math.min(4,2,6,8,5));
+// console.log(Math.max(4,2,6,8,5));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log((Math.random() * 10) + 1);
+
+const min = 5
+const max = 10
+
+console.log(Math.random() * (max - min + 1));
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
