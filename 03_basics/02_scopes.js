c02df09978c432fd50b5f292c557ea923828ec05
webdev2 <webdev2@digicians.com>
2025-01-05 23:35:57 +0530

    Global and Local scope is done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,11 @@
+// var c = 300
+let a = 200
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Local Scope:", a);
+}
+console.log("Global Scope:", a);
+// console.log(b);
+// console.log(c);
