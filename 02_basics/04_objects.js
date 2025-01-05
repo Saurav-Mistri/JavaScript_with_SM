69efb666a72d59518d48c156d768b8a596d1bcc0
webdev2 <webdev2@digicians.com>
2025-01-05 22:32:11 +0530

    object de-structure and JSON API intro done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -52,8 +52,33 @@ const users = [
     },
 ]
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); 
+
+const course = {
+    courseName: "JS in hindi",
+    price: "999",
+    courseInstructor: "Saurav Mistri"
+}
+
+// console.log(course.courseInstructor); // calling normal object item
+
+const { courseInstructor: instructor } = course
+
+// console.log(courseInstructor);
+// console.log(instructor); // distructure of the courseInstructor
+
+// json format
+
+// json in object format:
+// {
+//     "name": "Saurav",
+//     "courseName": "JS in Hindi",
+//     "price": "Free"
+// }
+
+// Refer the URL for the example: https://api.github.com/users/hiteshchoudhary
+
