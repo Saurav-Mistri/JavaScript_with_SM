bc769d1a03b1b22f7a1d6a96fd9c86347a85e44b
webdev2 <sauravmistri21@gmail.com>
2025-01-06 08:47:46 +0530

    scope is done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,32 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("Local Scope:", a);
+    // console.log("Local Scope:", a);
 }
-console.log("Global Scope:", a);
+// console.log("Global Scope:", a);
 // console.log(b);
 // console.log(c);
+
+function funOne() {
+    const userName = "Mr SM"
+
+    function funTwo() {
+        const website = "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    funTwo()
+}
+
+// funOne()
+
+if (true) {
+    const userName = "Saurav"
+    if (userName === "Saurav") {
+        const website = " JSInYouTube"
+        // console.log(userName + website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
