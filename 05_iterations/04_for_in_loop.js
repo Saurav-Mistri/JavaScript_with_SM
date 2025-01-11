9800f7c2e08ca864adb8a9e9551f71fdc411d131
webdev2 <sauravmistri21@gmail.com>
2025-01-11 15:56:36 +0530

    for-in loop in js is done

---
+++ 05_iterations/04_for_in_loop.js
@@ -0,0 +1,30 @@
+// const myObj = {
+//     js: 'JavaScript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swift by apple'
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// map.set('FR', "Fransisco")
+// map.set('IN', "India")
+
+// not execute
+// for (const key in object) {
+//     console.log(key);
+// }
