46c28939fa9001fa32bfa85907363967e833e874
webdev2 <sauravmistri21@gmail.com>
2025-01-11 12:54:47 +0530

    for loop in js done

---
+++ 05_iterations/one.js
@@ -0,0 +1,46 @@
+// for loop
+
+// for (let i = 0; i < 5; i++) {
+//     const element = i;
+//     if (i == 3) {
+//         console.log("index is reached at 3");
+//     }
+//     console.log(element);
+// }
+// console.log(element);
+
+// tables 1-10
+// for (let i = 1; i < 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j < 10; j++) {
+//         // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + '=' + i * j);
+//     }
+// }
+
+// let myArr = ["IronMan", "Spiderman", "Hulk", "Locky"]
+// console.log(myArr.length);
+
+// for (let i = 0; i <= myArr.length; i++) {
+//     const element = myArr[i];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected ${i}`);
+//         break;
+//     }
+//     console.log(`Value is i is ${i}`);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected ${i}`);
+//         continue;
+//     }
+//     console.log(`Value is i is ${i}`);
+// }
+
