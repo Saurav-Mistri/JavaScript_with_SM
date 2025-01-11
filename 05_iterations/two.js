3c20d85543290cf6738ed7d1d98469d07e1173b7
webdev2 <sauravmistri21@gmail.com>
2025-01-11 13:03:31 +0530

    while and do-while loop in js is done

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loop with variables
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// while loop with array
+
+// let myArr = ["IronMan", "Spiderman", "Hulk", "Locky"]
+// let i = 0
+
+// while (i < myArr.length) {
+//     console.log(`Value is ${myArr[i]}`);
+//     i = i + 1;
+// }
+
+// let score = 11;
+// do {
+//     console.log(`Score is ${11}`);
+//     score++;
+// } while (score <= 10);
+
