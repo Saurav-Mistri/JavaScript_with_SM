2ff4b1e67e28e8d35c4176d9d486ce86f57be58a
webdev2 <sauravmistri21@gmail.com>
2025-01-11 11:39:15 +0530

    switch in js is done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,24 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        // case "jan":
+        console.log("January");
+        break;
+    case 2:
+        // case "feb":
+        console.log("February");
+        break;
+    case 3:
+        // case "mar":
+        console.log("March");
+        break;
+    case 4:
+        // case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Please enter valid value!");
+        break;
+}
