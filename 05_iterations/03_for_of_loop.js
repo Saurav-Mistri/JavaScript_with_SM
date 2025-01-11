32ca6376ff48c14efc2165f348e3dc22fbb102c9
webdev2 <sauravmistri21@gmail.com>
2025-01-11 15:56:13 +0530

    for-of loop in js is done

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,47 @@
+// for of
+
+// ["", "", "", ""]
+// [{}, {}, {}]
+
+// const myArr1 = [1, 2, 3, 4, 5]
+
+// for (const num of myArr1) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello world!"
+
+// for (const greet of greetings) {
+//     if (greet === " ") {
+//         console.log(greetings[greet]);
+//         // break;
+//         continue;
+//     }
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// map.set('FR', "Fransisco")
+// map.set('IN', "India")
+
+// console.log(map);
+
+// for (const [key, val] of map) {
+//     console.log(key, ':=>', val);
+// }
+// const myObj = {
+//     'game1':'NFS',
+//     'game2':'Spiderman',
+//     'game3':'Mario'
+// }
+
+// console.log(myObj);
+
+// for (const [key, val] of myObj) {
+//     console.log(key, ':=>', val);
+// }
